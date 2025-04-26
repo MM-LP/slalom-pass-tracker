787ec39794abf03773e8e7399101cb767e09f299
Larry Prong <larry.prong@metamarine.ca>
2025-04-26 11:11:43 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 // main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import App from './app.jsx';
 import './index.css';
 
 // Register service worker for PWA support
